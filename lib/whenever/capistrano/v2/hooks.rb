d29d80473e7482cc74f317557989abb3fc1415b7
Justin Mazzi <jmazzi@gmail.com>
2014-02-25 10:22:43 -0500

    Use write_crontab

--- lib/whenever/capistrano/v2/hooks.rb
+++ lib/whenever/capistrano/v2/hooks.rb
@@ -2,7 +2,7 @@ require "whenever/capistrano/v2/recipes"
 
 Capistrano::Configuration.instance(:must_exist).load do
   # Write the new cron jobs near the end.
-  before "deploy:finalize_update", "whenever:update_crontab"
+  before "deploy:finalize_update", "whenever:write_crontab"
   # If anything goes wrong, undo.
-  after "deploy:rollback", "whenever:update_crontab"
+  after "deploy:rollback", "whenever:write_crontab"
 end
